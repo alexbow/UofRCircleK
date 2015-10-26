f23c567db713a49bd322a00b80e2fbc964106ac9
Alex <cousincuey>
2015-10-25 21:29:46 -0400

    Added the router

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 var http = require("http");
 var express = require('express');
+var app = express();
+var path = require("path");
 
-http.createServer(function (request, response) {
+app.use(express.static('./app'));
 
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   
-   // Send the response body as "Hello World"
-   response.end('Hello World\n');
-}).listen(3000);
+app.get('/', function(req, res){
+	res.sendFile(path.join('index.html'));
+});
 
-// Console will print the message
+app.get('/calendar.html', function(req, res){
+	res.sendFile(path.join('/calendar.html'));
+});
+
+app.listen(3000);
+
 console.log('Server running at http://127.0.0.1:3000/');
