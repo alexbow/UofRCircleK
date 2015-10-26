2ef58d2c52f1cf23e86c9f16d2f80a42afc2a08a
Alex <cousincuey>
2015-10-25 21:52:41 -0400

    added the base for all the pages along with the navigation

--- server.js
+++ server.js
@@ -13,6 +13,18 @@ app.get('/calendar.html', function(req, res){
 	res.sendFile(path.join('/calendar.html'));
 });
 
+app.get('/gallery.html', function(req, res){
+	res.sendFile(path.join('/gallery.html'));
+});
+
+app.get('/blog.html', function(req, res){
+	res.sendFile(path.join('/blog.html'));
+});
+
+app.get('/suggestions.html', function(req, res){
+	res.sendFile(path.join('/suggestions.html'));
+});
+
 app.listen(3000);
 
 console.log('Server running at http://127.0.0.1:3000/');
